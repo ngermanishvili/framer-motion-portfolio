10a972d02a89e22c4af7080669717ed0e6be6c89
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-11 04:33:02 +0400

    Add Personal Logo onscroll hide and top0 visible

--- src/components/Monogram/Monogram.js
+++ src/components/Monogram/Monogram.js
@@ -1,19 +1,33 @@
-import { forwardRef, useId } from 'react';
-import { classes } from 'utils/style';
+import React, { useRef, useEffect } from 'react';
 import styles from './Monogram.module.css';
 
-export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => {
-  const id = useId();
-  const clipId = `${id}monogram-clip`;
+export const Monogram = () => {
+  const monogramRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const monogramElement = monogramRef.current;
+      const isMobile = window.innerWidth <= 768; // Adjust the breakpoint as needed
+      const scrollY = window.scrollY;
+
+      if (isMobile && scrollY === 0) {
+        monogramElement.style.visibility = 'visible';
+      } else {
+        monogramElement.style.visibility = 'hidden';
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
-    <div
-      aria-hidden
-      width="46"
-      height="29"
-    >
+    <div aria-hidden width="46" height="29" className={styles.monogram} ref={monogramRef}>
       <h2 className={styles.monoh1}>Nika Germanishvili</h2>
-
     </div>
   );
-});
+};
+
+
+
+
