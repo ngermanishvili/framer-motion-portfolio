26eba28e2ae8b4d9d576b016c3ce7f101870eeed
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-11 04:39:08 +0400

    add apos syntax

--- src/layouts/Home/Profile.js
+++ src/layouts/Home/Profile.js
@@ -1,13 +1,9 @@
 import profileKatakana from 'assets/katakana-profile.svg?url';
-import profileImgLarge from 'assets/profile-large.jpg';
-import profileImgPlaceholder from 'assets/profile-placeholder.jpg';
-import profileImg from 'assets/profile.jpg';
 import { Button } from 'components/Button';
 import { DecoderText } from 'components/DecoderText';
 import { Divider } from 'components/Divider';
 import { Heading } from 'components/Heading';
 import { Image } from 'components/Image';
-import { Link } from 'components/Link';
 import { Section } from 'components/Section';
 import { Text } from 'components/Text';
 import { Transition } from 'components/Transition';
@@ -23,20 +19,20 @@ const ProfileText = ({ visible, titleId }) => (
       <DecoderText text="Hi there" start={visible} delay={500} />
     </Heading>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
-      Hey there, world! I'm Nika Germanishvili 22-year-old - Junior Front-End Developer. living life to the fullest in the vibrant Country Georgia, Tbilisi. Passionate, driven, and brimming with potential, I'm on a mission to make my mark in the world of web development.
+      Hey there, world! I&apos;m Nika Germanishvili 22-year-old - Junior Front-End Developer. living life to the fullest in the vibrant Country Georgia, Tbilisi. Passionate, driven, and brimming with potential, I&apos;m on a mission to make my mark in the world of web development.
 
-      I've certificate in front-end technologies from the esteemed Academy of Digital Industry. But that's just the beginning of my journey. I've taken it upon myself to dive headfirst into the vast ocean of knowledge, exploring the depths of React through platforms like Udemy.
+      I&apos;ve certificate in front-end technologies from the esteemed Academy of Digital Industry. But that&apos;s just the beginning of my journey. I&apos;ve taken it upon myself to dive headfirst into the vast ocean of knowledge, exploring the depths of React through platforms like Udemy.
 
-      Right now, I'm busy conquering the realm of three.js, unlocking the power to create mind-blowing 3D graphics on the web.  I've harnessed the mighty Next.js, an unstoppable framework that brings my web applications to life with server-side rendering, automatic code splitting, and seamless deployment. It's like magic, but with lines of code.
+      Right now, I&apos;m busy conquering the realm of three.js, unlocking the power to create mind-blowing 3D graphics on the web.  I&apos;ve harnessed the mighty Next.js, an unstoppable framework that brings my web applications to life with server-side rendering, automatic code splitting, and seamless deployment. It&apos;s like magic, but with lines of code.
 
 
     </Text>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
-      I'm a motivated, hardworking team player who thrives on challenges. I fearlessly embrace each hurdle that comes my way, turning them into stepping stones to success. There's no peak too high for me to conquer, no project too daunting for my unstoppable spirit.
+      I&apos;m a motivated, hardworking team player who thrives on challenges. I fearlessly embrace each hurdle that comes my way, turning them into stepping stones to success. There&apos;s no peak too high for me to conquer, no project too daunting for my unstoppable spirit.
     </Text>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
 
-      So, if you're ready to join forces and witness the extraordinary. I'm here to bring your web development dreams to life, one pixel at a time.
+      So, if you&apos;re ready to join forces and witness the extraordinary. I&apos;m here to bring your web development dreams to life, one pixel at a time.
 
       Contact Me - nikagermanishvili5@gmail.com
     </Text>
